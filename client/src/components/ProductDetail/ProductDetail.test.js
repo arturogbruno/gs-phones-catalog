fad99e003bb8d5f66b80b9c2ed114ad666bf88ee
Arturo G. Bruno <arturogbruno@gmail.com>
2020-06-27 19:17:15 +0200

    Add tests for ProductDetail component

--- client/src/components/ProductDetail/ProductDetail.test.js
+++ client/src/components/ProductDetail/ProductDetail.test.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { BrowserRouter } from "react-router-dom";
+import { mount, shallow } from "enzyme";
 import { Provider } from "react-redux";
-import store from "../../redux/store";
+import configureMockStore from "redux-mock-store";
 import ProductDetail from "./ProductDetail";
 
 describe("testing ProductDetail component", () => {
     let wrapper;
+    let store;
+    
     beforeEach(() => {
         const match = {
             params: {
@@ -13,15 +16,50 @@ describe("testing ProductDetail component", () => {
             }
         };
 
-        wrapper = shallow(
-            <Provider store={store}>
-                <ProductDetail match={match} />
-            </Provider>
-            );
-    });
+        const mockStore = configureMockStore();
 
+        store = mockStore({
+            products: [
+                { 
+                    id: 2,
+                    name: "iPhone 7"
+                }
+            ]
+        });
+
+        wrapper = mount(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <ProductDetail match={match} />
+                </BrowserRouter>
+            </Provider>
+        );
+    });
 
     it("should render ProductDetail component", () => {
         expect(wrapper).toMatchSnapshot();
+    });
+
+    it("should render ProductDetail and productData elements if product is found", () => {
+        expect(wrapper.find("ProductDetail").length).toEqual(1);
+        expect(wrapper.find(".productData").exists()).toBe(true);
+    });
+
+    it("should render NotFound component if product is not found", () => {
+        const match = {
+            params: {
+                id: "13"
+            }
+        };
+        
+        wrapper = mount(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <ProductDetail match={match} />
+                </BrowserRouter>
+            </Provider>
+        );
+
+        expect(wrapper.find("NotFound").length).toEqual(1);
     });
 });
