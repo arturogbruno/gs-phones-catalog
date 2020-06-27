b6647cff9f9b5cd24970862ad2522206abdc999c
Arturo G. Bruno <arturogbruno@gmail.com>
2020-06-27 10:36:05 +0200

    Add render test to App and change image on NotFound component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,1 +1,15 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import App from "./App";
 
+describe("testing App", () => {
+    it("should render App", () => {
+        expect(shallow(
+            <Provider store={store}>
+                <App />
+            </Provider>
+        )).toMatchSnapshot();
+    });
+});
