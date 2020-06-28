f1884b336d47eef5ae7d7cc91101c36aab57d306
Arturo G. Bruno <arturogbruno@gmail.com>
2020-06-28 21:19:52 +0200

    Add docker config

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addProducts } from "./redux/actions";
-import PhonesServices from "./services/PhonesServices";
+import axios from "axios";
 import Catalog from "./components/Catalog/Catalog";
 import ProductDetail from "./components/ProductDetail/ProductDetail";
 import Navbar from "./components/Navbar/Navbar";
@@ -13,15 +13,13 @@ import "./App.scss";
 function App() {
     const category = "phones";
 
-    const phonesServices = new PhonesServices();
-
     const dispatch = useDispatch();
     const products = useSelector(state => state.products);
 
     useEffect(() => {
         const getProductsData = async () => {
-            const phones = await phonesServices.getAllPhones();
-            dispatch(addProducts(phones));
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/phones`);
+            dispatch(addProducts(response.data));
         }
         getProductsData();
     }, [])
